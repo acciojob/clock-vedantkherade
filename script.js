2d54fb574f62ef9c0f7388b7ee6e5b6b168adb24
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-10-25 17:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+// let timer = document.getElementById("timer");
+
+// setInterval(() => {
+// 	let x = new Date();
+// 	timer.textContent = x.toLocaleString();
+// })
